e59f4db5997b612a69e2d0cba355f6f177179ef4
708-U <708.u.biz@gmail.com>
2020-05-05 16:26:35 +0900

    add basic types to type interface

---
+++ src/welcome/typeInterface/typeInterface.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	v := 42
+	x := 3.14
+	y := 0.1 + 0.5i
+	fmt.Printf("v is of type %T\n", v)
+	fmt.Printf("v is of type %T\n", x)
+	fmt.Printf("v is of type %T\n", y)
+}
